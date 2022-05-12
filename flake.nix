4412c66622540800126cee891d63f2cf1e9e7139
hugosenari <hugosenari@gmail.com>
2022-05-12 11:52:07 -0300

    chore: re index github-iffy-asynctools

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."asynctools-master".dir   = "master";
+  inputs."asynctools-master".owner = "nim-nix-pkgs";
+  inputs."asynctools-master".ref   = "master";
+  inputs."asynctools-master".repo  = "asynctools";
+  inputs."asynctools-master".type  = "github";
+  inputs."asynctools-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."asynctools-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
